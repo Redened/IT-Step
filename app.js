7432adb74c3133de346c86eae70a7962ee72f93d
Redened <85760145+Redened@users.noreply.github.com>
2022-02-06 20:08:35 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 $(document).ready(function () {
 
-     $(".next").click(function () {
-          $(".active-step").parent().next().find(".step").addClass("active-step").parent().prevAll().find(".step").removeClass("active-step");
-          $(".active-option").next().addClass("active-option").prevAll().removeClass("active-option");
+  $(".next").click(function () {
+       $(".active-step").parent().next().find(".step").addClass("active-step").parent().prevAll().find(".step").removeClass("active-step");
+       $(".active-option").next().addClass("active-option").prevAll().removeClass("active-option");
 
-          $(".resultbtn").text($("#text").val())
-          $(".resultbtn").css({backgroundColor: `${$("#bcolor").val()}`})
-     });
+       $(".resultbtn").text($("#text").val())
+       $(".resultbtn").css({backgroundColor: `${$("#bcolor").val()}`})
+  });
 
-     $(".prev").click(function () {
-          $(".active-step").parent().prev().find(".step").addClass("active-step").parent().nextAll().find(".step").removeClass("active-step");   
-          $(".active-option").prev().addClass("active-option").nextAll().removeClass("active-option");
-     })
+  $(".prev").click(function () {
+       $(".active-step").parent().prev().find(".step").addClass("active-step").parent().nextAll().find(".step").removeClass("active-step");   
+       $(".active-option").prev().addClass("active-option").nextAll().removeClass("active-option");
+  })
 
 
 });
@@ -22,11 +22,10 @@ $(document).ready(function () {
 var tooltip = document.querySelectorAll('.slider-tooltip');
 
 document.addEventListener('mousemove', function(This) {
-     for (var X = tooltip.length; X--;) {
-          tooltip[X].style.left = (This.pageX - 25) + 'px';
-          tooltip[X].style.top = (This.pageY - 65) + 'px';
-          document.querySelector('.slider-tooltip h4').innerHTML = `${document.querySelector('#range').value}`
-      
-     }
+  for (var X = tooltip.length; X--;) {
+       tooltip[X].style.left = (This.pageX - 25) + 'px';
+       tooltip[X].style.top = (This.pageY - 65) + 'px';
+       document.querySelector('.slider-tooltip h5').innerHTML = `${document.querySelector('#range').value}`
+   
+  }
 });
-
